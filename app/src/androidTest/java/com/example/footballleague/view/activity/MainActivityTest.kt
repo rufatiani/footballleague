105a7a00dd0718f21798f835c7dfe6caded8b2ed
rufatiani <rufatiani@gmail.com>
2019-12-04 17:07:56 +0700

    Bug fix instrument test

---
+++ app/src/androidTest/java/com/example/footballleague/view/activity/MainActivityTest.kt
@@ -0,0 +1,118 @@
+package com.example.footballleague.view.activity
+
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.example.footballleague.R
+import com.example.footballleague.adapter.EventAdapter
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.`is`
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun mainActivityTest() {
+        val bottomNavigationItemView = onView(
+            allOf(
+                withId(R.id.navigation_events), withContentDescription("Events"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(R.id.nav_view2),
+                        0
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        bottomNavigationItemView.perform(click())
+
+        val appCompatImageView = onView(
+            allOf(
+                withClassName(`is`("androidx.appcompat.widget.AppCompatImageView")),
+                withContentDescription("Search"),
+                childAtPosition(
+                    allOf(
+                        withClassName(`is`("android.widget.LinearLayout")),
+                        childAtPosition(
+                            withId(R.id.svEvents),
+                            0
+                        )
+                    ),
+                    1
+                ),
+                isDisplayed()
+            )
+        )
+        appCompatImageView.perform(click())
+
+        val searchAutoComplete2 = onView(
+            allOf(
+                withId(R.id.svEvents),
+                isDisplayed()
+            )
+        )
+        searchAutoComplete2.perform(typeText("england"), closeSoftKeyboard())
+
+        val searchAutoComplete3 = onView(
+            allOf(
+                withText("england"),
+                isDisplayed()
+            )
+        )
+        searchAutoComplete3.perform(pressImeActionButton())
+
+        val recyclerView = onView(
+            allOf(
+                withClassName(`is`("androidx.recyclerview.widget.RecyclerView")),
+                withId(R.id.rvEventList),
+                isDisplayed()
+            )
+        )
+
+        recyclerView.perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(1))
+        Thread.sleep(1000)
+        recyclerView.perform(actionOnItemAtPosition<RecyclerView.ViewHolder>(1, click()))
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
